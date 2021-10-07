063a67f4feb57a3d53da6f3703cde5cdc8c198bf
Jesse <jesse>
2021-10-06 18:44:19 -0700

    use raspi-config

--- setup.sh
+++ setup.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-if [ "$#" -ne 1 ]
-then
-  echo "Usage: ./setup desired-hostname"
-  exit 1
-fi
+#if [ "$#" -ne 1 ]
+#then
+#  echo "Usage: ./setup arg1"
+#  exit 1
+#fi
 
 echo 'alias ll="ls -la"' >> ~/.bash_aliases
 echo '' >> ~/.bash_aliases
-
-sudo echo $1 >> /etc/hostname
 
 sudo apt update
 sudo apt upgrade -y
 sudo apt install git docker.io docker-compose dnsutils -y
 sudo usermod -aG docker ${USER}
+
+sudo raspi-config
+
 sudo reboot now
