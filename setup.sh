9d9d46ff20839857c4959bd758844fad63a623f5
Jesse <jesse>
2021-10-10 21:37:26 -0700

    udpate

--- setup.sh
+++ setup.sh
@@ -17,21 +17,23 @@ while getopts 'cd' flag; do
 done
 
 # Aliases
-if [clean != 'true']; then
+if [ clean != 'true' ]; then
   cp ~/.bash_aliases ~/.bash_aliases.orig
 fi
+
 cp ./bash_aliases ~/.bash_aliases
 
 # SSH hardening
-if [clean != 'true']; then
+if [ clean != 'true' ]; then
   sudo mv /etc/ssh/sshd_config /etc/ssh/sshd_config.orig
 fi
+
 sudo cp ./sshd_config /etc/ssh/sshd_config
 
 sudo apt update && sudo apt upgrade -y
 
 # Docker
-if [docker == 'true']; then
+if [ docker == 'true' ]; then
   sudo apt install git docker.io docker-compose dnsutils -y
   sudo usermod -aG docker ${USER}
 fi
