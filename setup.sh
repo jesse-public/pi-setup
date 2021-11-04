54a76a7ce7b61af97b1c6063d27024d11b071354
Jesse <jesse>
2021-11-03 23:42:19 -0700

    vlan

--- setup.sh
+++ setup.sh
@@ -2,15 +2,17 @@
 
 clean=''
 docker=''
+vlan=''
 
 print_usage() {
-  printf "Usage: ./setup.sh [-c (do not backup configuration files)] [-d (setup docker)]"
+  printf "Usage: ./setup.sh [-c (do not backup configuration files)] [-d (setup docker)] [-v (enable vlans)]"
 }
 
 while getopts 'cd' flag; do
   case "${flag}" in
     c) clean='true' ;;
     d) docker='true' ;;
+    v) vlan='true' ;;
     *) print_usage
        exit 1 ;;
   esac
@@ -32,6 +34,34 @@ sudo cp ./sshd_config /etc/ssh/sshd_config
 
 sudo apt update && sudo apt upgrade -y
 
+if [ vlan == 'true' ]; then
+  sudo apt install vlan
+  cat > /etc/network/interfaces.d/vlans << EOL
+# Update 10 to desired VLAN
+#auto eth0.10
+#iface eth0.10 inet manual
+#  vlan-raw-device eth0
+EOL
+  cat >> /etc/dhcpcd.conf << EOL
+# Static IP: LAN
+#interface eth0
+#static ip_address=192.168.0.10/24
+#static routers=192.168.0.1
+#static domain_name_servers=192.168.0.10 192.168.0.10
+
+# Static IP: VLAN 10
+#interface eth0.10
+#static ip_address=192.168.10.2/24
+#static routers=192.168.10.1
+#static domain_name_servers=
+EOL
+  echo "VLANS enabled, but not yet configured!"
+  echo "The following files must be configured to support the desired VLANs:"
+  echo "  * /etc/network/interfaces.d/vlans"
+  echo "  * /etc/dhcpcd.conf"
+  echo "Then reboot and run hostname -I to verify changes"
+fi
+
 # Docker
 if [ docker == 'true' ]; then
   sudo apt install git docker.io docker-compose dnsutils -y
@@ -41,7 +71,7 @@ if [ docker == 'true' ]; then
 fi
 
 # RPI config
-sudo raspi-config
+#sudo raspi-config
 
 # static IP
 echo "Remember to edit /etc/dhcpcd.conf to set static IP and DNS"
